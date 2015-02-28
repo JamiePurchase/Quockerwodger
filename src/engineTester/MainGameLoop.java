076a45c7b90ac71d62febdb94033892275d04467
Jamie Purchase <jamie.purchase@medicapp.com>
2015-02-28 23:02:31 +0000

    VAOs and VBOs
    https://www.youtube.com/watch?v=WMiggUPst-Q

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -3,16 +3,40 @@ package engineTester;
 import org.lwjgl.opengl.Display;
 
 import renderEngine.DisplayManager;
+import renderEngine.Loader;
+import renderEngine.RawModel;
+import renderEngine.Renderer;
 
 public class MainGameLoop
 {
 	public static void main(String[] args)
 	{
 		DisplayManager.createDisplay();
+		
+		Loader loader = new Loader();
+		Renderer renderer = new Renderer();
+		
+		float[] vertices = {
+				// Left Bottom Triangle
+				-0.5f, 0.5f, 0f,
+				-0.5f, -0.5f, 0f,
+				0.5f, -0.5f, 0f,
+				// Right Top Triangle
+				0.5f, -0.5f, 0f,
+				0.5f, 0.5f, 0f,
+				-0.5f, 0.5f, 0f
+		};
+		
+		RawModel model = loader.loadToVAO(vertices);
+		
 		while(!Display.isCloseRequested())
 		{
+			renderer.prepare();
+			renderer.render(model);
 			DisplayManager.updateDisplay();
 		}
+		
+		loader.cleanUp();
 		DisplayManager.closeDisplay();
 	}
 }
